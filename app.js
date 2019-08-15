995fc79b83fdbf53b87e3bf3811c8adcc649e401
Daniel Whiteman <daniel.whiteman1@gmail.com>
2019-08-15 15:36:35 -0500

    Middleware
    Added middleware plus routes and various dynami links accross pages.

--- app.js
+++ app.js
@@ -1,21 +1,68 @@
 /* command to run nodemon in terminal
-   npm run dev-start */
+   npm run dev-start
+
+Additional Resources Used: 'Learning Node.js Development'by Andrew Mead,
+'Express In Action' by Evan Hahn
+*/
+
 
 console.log('Starting app.js');
 
+//Vars
 const pug = require('pug');
 const express = require('express');
-const data = require('data.json');
-//const fs = require('fs');
 const app = express();
+
+const data = require("./data.json");
+const projects = data.projects
+
+app.set('view engine', 'pug');
+app.use('/static', express.static('public'));
+
+
 const port = 3000;
 
-app.get('/', (req, res) => res.send('<h1>This is my home page fool</h1>'))
 
-app.get('/index', (req, res) => res.send('This is my index page'))
 
-app.get('/about', (req, res) => res.send('This is my about page'))
 
-app.get('/projects', (req, res) => res.send('This is my dynamic projects page'))
 
+
+//Home Page
+app.get('/', (req, res) => {
+  res.render('index', {projects: projects});
+});
+
+
+//About Page
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+
+//Dynamic Projects Route
+app.get('/projects/:id', (req, res) => {
+  const id = req.params.id
+  const project = projects[id];
+  res.render('project', {project});
+});
+
+
+
+
+
+//Error Handlers
+// 404
+app.use((req, res, next) => {
+  return res.status(404).send({ message: 'Route'+req.url+' Not found.' });
+});
+
+// 500 - Any server error
+app.use((err, req, res, next) => {
+  return res.status(500).send({ error: err });
+});
+
+
+
+
+//Server port
 app.listen(port, () => console.log(`Unit 6 portfolio app listening on port ${port}!`))
